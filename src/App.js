7f6a2177bb3ba47a460e235a34fb6088df615c06
Olaife Olawore <olaifeolawore@gmail.com>
2020-10-22 09:26:18 +0100

    Added react-router-dom to the mix and tested routing with a page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 class App extends Component {
   render() {
-  return <div className='App'><HomePage /></div>;
+  return (
+      
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component = {HomePage}></Route>
+          <Route exact path='/shop/hats' component = {HatsPage}></Route>
+        </Switch>
+      </div>
+    );
   }
 }
 
